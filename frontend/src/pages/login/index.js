64c47e3d65f716949d6220366c0db5d3838e1ccb
Phạm Nhật Tiến <83960491+1wku@users.noreply.github.com>
2022-02-17 22:33:50 +0700

    setup layout

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -7,6 +7,7 @@ export default function Login() {
     return (
         <div className="container">
             <img src={banner} alt="" className={styles.container} />
+            <h1>oke oke ok1</h1>
         </div>
     )
 }
