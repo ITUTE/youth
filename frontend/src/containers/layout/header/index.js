3655f90b876752a6ed318bfa5030d916f9b21318
Quang Huy <ngoquanghuy2210@gmail.com>
2022-02-18 12:25:13 +0700

    Create--header

--- frontend/src/containers/layout/header/index.js
+++ frontend/src/containers/layout/header/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
-
-import styles from './index.module.scss'
-
+import styles from'./index.module.scss'
+import {ReactComponent as Reactlogo} from '../../../assets/IconSearch.svg'
 export default function Header() {
-    return <div className={styles.test}>Header</div>
+    return [
+        <div className={styles.header}>
+            <span className={styles.homePage}> YOUTH FIT</span>
+            <div className={styles.search}>
+                <div className={styles.iconSearch}><Reactlogo /></div>
+                <input type="text" name="seach" className={styles.input} placeholder="Nhập từ khóa"></input>
+            </div>
+             <div className={styles.container}> Trang chủ </div>
+            <div className={styles.container}> Văn phòng điện tử </div>
+            <div className={styles.container}> CLB/Đội/Nhóm  </div>
+            <div className={styles.container}> SV5T</div>
+            <div className={styles.container}> Hỗ trợ sinh viên </div>
+            <div className={styles.container}> Vinh danh </div>
+            <div className={styles.container}> Về chúng tôi </div>
+        </div>
+        
+    ]
 }
