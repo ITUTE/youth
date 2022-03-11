ef3944ea5f1a78e9a793e7a2957fb47d4f94f422
Phạm Nhật Tiến <83960491+1wku@users.noreply.github.com>
2022-03-11 22:25:02 +0700

    config office route

---
+++ frontend/src/pages/office/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//import Sidebar from './sidebar'
+//import Body from './body'
+import styles from './index.module.scss'
+
+function Office() {
+    return (
+        <div className={styles.container}>
+            <div className="left">left</div>
+            <div className="right">right</div>
+        </div>
+    )
+}
+
+export default Office
