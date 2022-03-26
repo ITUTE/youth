29ff3b56b5673ea6b54be68a33918feaf7f37fb5
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-03-26 13:28:11 +0700

    add `About`

---
+++ frontend/src/pages/about/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <iframe title="API"
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRM9rIxlKRrB802c-HbJytb9Ap6g-nVI8Fer0y1JclmjPWJk_6ZOx60kSFXf_gxnZtfkfcnNyBcOCy6/pubhtml?widget=true&amp;headers=false">
+        </iframe>
+    )
+}
