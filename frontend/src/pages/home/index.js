7cfb7ea417e560ff48b74a4aa6c119f6ec9487d1
TienNHM.Utop <TienNHM@utop.io>
2023-05-27 01:28:42 +0700

    update `Đang diễn ra`

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -95,16 +95,20 @@ export default function Home() {
 
     const currEvents = [
         {
+            imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685123778/2022-2023/mit-2023_wi5e6f.webp',
+            // title: 'MASTERING IT 2023',
+        },
+        {
             imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1648146264/2021-2022/Hackathon/Hackathon-2022_awhnfg.png',
-            title: 'HACKATHON 2022',
+            // title: 'HACKATHON 2022',
         },
         {
             imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1648146366/2021-2022/Th%C3%A1ng%20thanh%20ni%C3%AAn/TTN_indytq.jpg',
-            title: 'THÁNG THANH NIÊN 2022',
+            // title: 'THÁNG THANH NIÊN 2022',
         },
         {
             imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1648146506/2021-2022/Tuy%C3%AAn%20d%C6%B0%C6%A1ng%20SV5T%20-%20IT%20Show/SV5T-IT_Show_dtuand.jpg',
-            title: 'LỄ TUYÊN DƯƠNG SINH VIÊN NĂM TỐT - IT SHOW',
+            // title: 'LỄ TUYÊN DƯƠNG SINH VIÊN NĂM TỐT - IT SHOW',
         },
     ]
 
