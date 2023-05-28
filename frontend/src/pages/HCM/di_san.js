4eb2077a9ac1dbcc7e785deb815cd6916dc0a824
TienNHM.Utop <TienNHM@utop.io>
2023-05-28 13:49:06 +0700

    add KGVH HCM: tiểu sử, sách

---
+++ frontend/src/pages/HCM/di_san.js
@@ -0,0 +1,78 @@
+export const SECTION_TITLE = 'BỘ SÁCH "DI SẢN HỒ CHÍ MINH"'
+
+export const SLIDERS = [
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685256118/2022-2023/di-san/DS0_m9mwt4.png',
+        title: '',
+        content: ''
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685256118/2022-2023/di-san/DS0_m9mwt4.png',
+        title: '',
+        content: ''
+    },
+]
+
+export const ITEMS = [ 
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255574/2022-2023/di-san/DS1_ktq4h7.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255592/2022-2023/di-san/DS2_jy4yxn.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255593/2022-2023/di-san/DS3_ecjsnf.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255582/2022-2023/di-san/DS4_ywslt5.png',
+        title: '',
+        sid: ''
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255585/2022-2023/di-san/DS5_na3qzx.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255581/2022-2023/di-san/DS6_zx7ifd.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255581/2022-2023/di-san/DS7_eaodg2.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255577/2022-2023/di-san/DS8_qymq2f.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255584/2022-2023/di-san/DS9_vho0ob.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255593/2022-2023/di-san/DS10_ye0g37.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255596/2022-2023/di-san/DS11_p0db53.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255576/2022-2023/di-san/DS12_wv9puc.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255574/2022-2023/di-san/DS13_xfycgq.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255586/2022-2023/di-san/DS14_zaxen7.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685255577/2022-2023/di-san/DS15_b9offc.png',
+        title: '',
+    },
+]
---
+++ frontend/src/pages/HCM/tieu_su.js
@@ -0,0 +1,46 @@
+export const SECTION_TITLE = 'TIỂU SỬ'
+
+export const SLIDERS = [
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685254752/2022-2023/HCM/%E1%BA%A2NH_B%C3%8CA_PAGE_B%C3%81C_H%E1%BB%92_FIX_LOGO_cn2who.png',
+        title: '',
+        content: ''
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685254752/2022-2023/HCM/%E1%BA%A2NH_B%C3%8CA_PAGE_B%C3%81C_H%E1%BB%92_FIX_LOGO_cn2who.png',
+        title: '',
+        content: ''
+    },
+]
+
+export const ITEMS = [ 
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685247897/2022-2023/HCM/tieu-su-hcm_1_a1iqnw.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685247892/2022-2023/HCM/tieu-su-hcm_2_lemcnf.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685247893/2022-2023/HCM/tieu-su-hcm_3_hg7l6l.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685247897/2022-2023/HCM/tieu-su-hcm_4_e5ctla.png',
+        title: '',
+        sid: ''
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685247892/2022-2023/HCM/tieu-su-hcm_5_qhriux.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685247892/2022-2023/HCM/tieu-su-hcm_6_pbqkn9.png',
+        title: '',
+    },
+    {
+        imgUrl: 'https://res.cloudinary.com/yitute/image/upload/v1685247892/2022-2023/HCM/tieu-su-hcm_7_ptxc6v.png',
+        title: '',
+    },
+]
